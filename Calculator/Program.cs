7b6368098200b2fb0f82e73bfc2490cc8f949bed
Mu4lka <shatnykhmax@mail.ru>
2022-09-16 20:28:57 +0300

    Изменил 41 строчку

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -38,7 +38,7 @@ namespace Calculator
             string Sing = Console.ReadLine();
             if (!(Sing == "+" ^ Sing == "-" ^ Sing == "*" ^ Sing == "/"))
             {
-                Console.WriteLine("Неизвестный знак!");
+                Console.WriteLine("Неверный знак");
             }
             else
             {
