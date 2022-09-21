f868e53456c281b2b5acb46dc49d0aa41c39d8cc
Mu4lka <shatnykhmax@mail.ru>
2022-09-21 20:00:16 +0300

    Поправил стилистику кода(поправил имя переменных)

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,59 +5,59 @@ namespace Calculator
 {
     internal class Program
     {
-        public static void Calculate(double FirstNumber, string Sing, double SecondNumber)
+        public static void Calculate(double firstNumber, string sing, double secondNumber)
         {
-            if (Sing == "+")
+            if (sing == "+")
             {
-                Console.WriteLine(FirstNumber + SecondNumber);
+                Console.WriteLine(firstNumber + secondNumber);
             }
-            if (Sing == "-")
+            if (sing == "-")
             {
-                Console.WriteLine(FirstNumber - SecondNumber);
+                Console.WriteLine(firstNumber - secondNumber);
             }
-            if (Sing == "*")
+            if (sing == "*")
             {
-                Console.WriteLine(FirstNumber * SecondNumber);
+                Console.WriteLine(firstNumber * secondNumber);
             }
-            if (Sing == "/")
+            if (sing == "/")
             {
-                if (SecondNumber == 0)
+                if (secondNumber == 0)
                 {
                     Console.WriteLine("На ноль делить нельзя!");
                 }
                 else
                 {
-                    Console.WriteLine(FirstNumber / SecondNumber);
+                    Console.WriteLine(firstNumber / secondNumber);
                 }
             }
         }
 
         public static void Main()
         {
-            double FirstNumber;
-            double SecondNumber;
+            double firstNumber;
+            double secondNumber;
             try
             {
-                FirstNumber = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                firstNumber = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             }
             catch
             {
                 Console.WriteLine("Первое число указано неверно!");
                 return;
             }
-            string Sing = Console.ReadLine();
-            if (Sing == "+" ^ Sing == "-" ^ Sing == "*" ^ Sing == "/")
+            string sing = Console.ReadLine();
+            if (sing == "+" ^ sing == "-" ^ sing == "*" ^ sing == "/")
             {
                 try
                 {
-                    SecondNumber = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    secondNumber = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                 }
                 catch
                 {
                     Console.WriteLine("Второе число указано неверно!");
                     return;
                 }
-                Calculate(FirstNumber, Sing, SecondNumber);
+                Calculate(firstNumber, sing, secondNumber);
             }
             else
             {
